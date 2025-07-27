2e91525e08251eff885f5e4b3e33fb391cb8dcaa
Braulio Robledo Delgado <braulio.robledo@correounivalle.edu.co>
2025-07-26 19:50:32 -0500

    implementacion de drag and drop para los barcos/creacion de clase GameStateManager para unificar los datos/y implementacion de metodos para resetteos

--- src/main/java/com/example/battleship_/HelloApplication.java
+++ src/main/java/com/example/battleship_/HelloApplication.java
@@ -2,8 +2,6 @@ package com.example.battleship_;
 
 import com.example.battleship_.view.MenuView;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
